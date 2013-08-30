cea29ed5cd90c7c2c38197c8d80d31b7fc843d4f
Matthew Tovbin <tovbinm@gmail.com>
2013-08-30 14:08:05 -0700

    Add signin page and asset caching

--- util_ounit.ml
+++ util_ounit.ml
@@ -1,3 +1,15 @@
+let assert_string_equal =
+  OUnit.assert_equal ~printer:(fun s -> Printf.sprintf "'%s'" s)
+
+let assert_int_equal =
+  OUnit.assert_equal ~printer:string_of_int
+
+let assert_float_equal =
+  OUnit.assert_equal ~printer:string_of_float
+
+let assert_bool_equal =
+  OUnit.assert_equal ~printer:string_of_bool
+
 let test description f =
   let open OUnit in
   description >:: (fun () -> assert_bool "failed" (f ()))
