f23d703ee5ccafbaa8fd6b1da2ebbd0bc6456c61
Martin Jambon <martin@mjambon.com>
2013-10-09 13:09:14 -0700

    Email forwarding and tracking, with basic interpretation of answers to multiple-choice questions.

---
+++ util_string.ml
@@ -0,0 +1,31 @@
+let ascii_lowercase s =
+  let s' = String.copy s in
+  for i = 0 to String.length s - 1 do
+    match s.[i] with
+    | 'A'..'Z' as c -> s'.[i] <- Char.chr (Char.code c + 32)
+    | _ -> ()
+  done;
+  s'
+
+let test_ascii_lowercase () =
+  ascii_lowercase "AbCd\xc9F" = "abcd\xc9f"
+
+let whitespace_rex = Pcre.regexp "[ \t\r\n]+"
+
+let compact_whitespace s =
+  let s = BatString.strip ~chars:" \t\r\n" s in
+  Pcre.substitute
+    ~rex: whitespace_rex
+    ~subst: (fun _ -> " ")
+    s
+
+let test_compact_whitespace () =
+  compact_whitespace " \ta bb \t \n\n ccc\n\t" = "a bb ccc"
+
+let ascii_normalize s =
+  compact_whitespace (ascii_lowercase s)
+
+let tests = [
+  "ascii lowercase", test_ascii_lowercase;
+  "compact whitespace", test_compact_whitespace;
+]
