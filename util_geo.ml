33cb881f95ae3306d4bc304f7e1035c791d4cf8b
Martin Jambon <martin@mjambon.com>
2013-11-04 00:23:42 -0800

    Fixed transit calculations

--- util_geo.ml
+++ util_geo.ml
@@ -32,21 +32,40 @@ let dot p1 p2 =
   +. p1.y *. p2.y
   +. p1.z *. p2.z
 
+let div { x; y; z } r = {
+  x = x /. r;
+  y = y /. r;
+  z = z /. r;
+}
+
 let arc_length_xyz p1 p2 =
   let angle = acos (dot p1 p2 /. (earth_radius ** 2.)) in
   earth_radius *. angle
+
+let arc_length_xyz p1 p2 =
+  if p1 = p2 then 0.
+  else
+    let angle = acos (dot p1 p2 /. (earth_radius *. earth_radius)) in
+    earth_radius *. angle
 
 let arc_length loc1 loc2 =
   arc_length_xyz (xyz_of_latlon loc1) (xyz_of_latlon loc2)
 
 
 let palo_alto = { lat = 37.429167; lon = -122.138056 }
-let san_francisco = { lat = 37.7833; lon = 122.4167 }
+let san_francisco = { lat = 37.7833; lon = -122.4167 }
 let new_york_city = { lat = 41.145556; lon = -73.995 }
 
 let test_arc_length () =
   let d = arc_length palo_alto new_york_city in
-  d >= 4000e3 && d <= 4200e3
+  assert (d >= 4000e3 && d <= 4200e3);
+  let d =
+    arc_length
+      palo_alto
+      { palo_alto with lon = palo_alto.lon +. 1e-9 }
+  in
+  assert (d > 0. && d < 1.);
+  true
 
 let tests = [
   "arc length", test_arc_length;
