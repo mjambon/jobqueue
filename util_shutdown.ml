f454347c0cb257a49ecce9fa90195f7e75ac69cd
Martin Jambon <martin@mjambon.com>
2015-03-03 15:45:33 -0800

    Clean shutdown procedure that handles SIGTERM, raises the Canceled exception in all threads, and gives them 5 seconds to finish their business.

---
+++ util_shutdown.ml
@@ -0,0 +1,39 @@
+open Lwt
+
+let shutdown_condition = Lwt_condition.create ()
+let shutting_down = ref false
+
+let is_shutting_down () = !shutting_down
+
+let shutdown grace_period =
+  if not !shutting_down then (
+    shutting_down := true;
+    Lwt_condition.signal shutdown_condition ();
+    (* Give other threads some time to finish what they were doing *)
+    Log.logf `Info "SHUTDOWN in %g seconds" grace_period;
+    ignore (
+      Lwt_unix.sleep grace_period >>= fun () ->
+      Log.logf `Info "STOP";
+      exit 0
+    )
+  )
+
+let shutdown_on_sigterm grace_period =
+  let _handler_id =
+    Lwt_unix.on_signal 15 (fun signum -> shutdown grace_period) in
+  ()
+
+let wait_for_shutdown () =
+  if is_shutting_down () then
+    return ()
+  else
+    Lwt_condition.wait shutdown_condition
+
+exception Shutdown
+
+let cancel_on_shutdown x =
+  ignore
+    (pick [ (x >>= fun y -> return ());
+            wait_for_shutdown () ]
+    );
+  x
---
+++ util_shutdown.mli
@@ -0,0 +1,29 @@
+exception Shutdown
+
+val is_shutting_down : unit -> bool
+
+val shutdown : float -> unit
+  (*
+     Signal all watcher threads that the system is shutting down,
+     wait for the given duration (seconds),
+     then exit the process.
+
+     Works only from within the lwt main loop.
+  *)
+
+val shutdown_on_sigterm : float -> unit
+  (*
+     Trigger a shutdown when the TERM signal is received.
+  *)
+
+val wait_for_shutdown : unit -> unit Lwt.t
+  (*
+     Create an lwt thread that returns when the system starts shutting down;
+     returns immediately if the system has already shutting down.
+  *)
+
+val cancel_on_shutdown : 'a Lwt.t -> 'a Lwt.t
+  (*
+     Terminate the given lwt thread with the Lwt.Canceled exception
+     when system is shutting down.
+  *)
