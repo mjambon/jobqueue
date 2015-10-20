515daefb86bcb310ac919a9341d05ba04166b11d
Martin Jambon <martin@mjambon.com>
2015-10-19 19:40:07 -0700

    Work in progress. Got rid of more hooks. Saving mysql lookups one at a time.

--- util_late_init.ml
+++ util_late_init.ml
@@ -16,31 +16,71 @@
 
    (* Module B
 
-      foo is initialized in this module. The value it takes is typically
-      a function
+      Function foo is initialized in this module.
    *)
    let foo x y =
      ...
 
-   let () = init_foo foo    (* <-- initialization *)
+   let () = A.init_foo foo    (* <-- initialization *)
 *)
+
+let uninitialized = Hashtbl.create 10
+
+let create_id =
+  let n = ref 0 in
+  fun () ->
+    let id = !n in
+    assert (id >= 0);
+    incr n;
+    id
 
 let create name =
   let r = ref None in
+  let id = create_id () in
+  Hashtbl.add uninitialized id name;
   let init f =
     match !r with
-    | None -> r := Some f
-    | Some _ -> failwith (name ^ " was already initialized")
+    | None ->
+        Hashtbl.remove uninitialized id;
+        r := Some f
+    | Some _ ->
+        failwith (name ^ " was already initialized")
   in
   let call x =
     match !r with
-    | None -> failwith ("Uninitialized function " ^ name)
-    | Some f -> f x
+    | None ->
+        failwith ("Uninitialized function " ^ name)
+    | Some f ->
+        f x
   in
   init, call
 
+(*
+   Return the list of uninitialized functions, sorted by creation date.
+*)
+let get_all_uninitialized () =
+  let l =
+    Hashtbl.fold (fun id name acc -> (id, name) :: acc) uninitialized []
+  in
+  let sorted = List.sort (fun (a, _) (b, _) -> compare a b) l in
+  List.map snd sorted
+
+(*
+   Print an error and exit the process if anything is uninitialized.
+*)
+let check_all () =
+  match get_all_uninitialized () with
+  | [] -> ()
+  | l ->
+      Printf.eprintf
+        "The following functions were not initialized: %s\n%!"
+        (String.concat ", " l);
+      exit 1
+
 let test_late_initialization () =
+  let n0 = List.length (get_all_uninitialized ()) in
   let init_foo, foo = create "foo" in
+  assert (List.length (get_all_uninitialized ()) = n0 + 1);
   (try
      ignore (foo 1 2);
      assert false
@@ -48,6 +88,7 @@ let test_late_initialization () =
 
   let real_foo x y = x + y in
   init_foo real_foo;
+  assert (List.length (get_all_uninitialized ()) = n0);
   (try
      init_foo real_foo;
      assert false
