155ace13e1d8a2f1daf86051b6a8375fe70b4d85
Martin Jambon <martin@mjambon.com>
2016-01-07 18:23:07 -0800

    Function to produce a count of labels actually used.

--- util_counter.ml
+++ util_counter.ml
@@ -20,5 +20,10 @@ let incr tbl k =
 let get tbl =
   let l = Hashtbl.fold (fun k r acc -> (k, !r) :: acc) tbl [] in
   let a = Array.of_list l in
-  Array.sort (fun (_, a) (_, b) -> compare b a) a;
+  Array.sort (fun (x1, (n1 : int)) (x2, n2) ->
+    let c = compare n2 n1 in
+    if c <> 0 then c
+    else
+      compare x1 x2
+  ) a;
   Array.to_list a
