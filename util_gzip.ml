47e58df1ad6958e73773c55eaed8ca7a18bf8f0e
Martin Jambon <martin@mjambon.com>
2016-04-26 15:25:08 -0700

    Marshal+gzip NB models for storage

--- util_gzip.ml
+++ util_gzip.ml
@@ -1,11 +1,19 @@
 (*
    Apparently we have to write this ourselves.
 *)
-let gunzip_string s =
+let decompress_string s =
   (* some arbitrary limit on input size *)
   if String.length s > 100_000_000 then
-    invalid_arg "Util_gzip.gunzip_string: input too large";
-  let filename = Filename.temp_file "wolverine-" ".gz" in
+    invalid_arg "Util_gzip.decompress_string: input too large";
+  let filename =
+    let temp_dir =
+      let in_memory_dir = "/dev/shm" in
+      if Sys.file_exists in_memory_dir then
+        Some in_memory_dir
+      else
+        None
+    in
+    Filename.temp_file ?temp_dir "wolverine-" ".gz" in
   let finally () = Sys.remove filename in
   try
     BatPervasives.output_file ~filename ~text:s;
@@ -28,10 +36,10 @@ let gunzip_string s =
     finally ();
     Trax.raise __LOC__ e
 
-let gzip_string s =
+let compress_string s =
   (* some arbitrary limit on input size *)
   if String.length s > 1_000_000_000 then
-    invalid_arg "Util_gzip.gzip_string: input too large";
+    invalid_arg "Util_gzip.compress_string: input too large";
   let filename = Filename.temp_file "wolverine-" ".gz" in
   let finally () = Sys.remove filename in
   try
@@ -45,21 +53,21 @@ let gzip_string s =
     finally ();
     Trax.raise __LOC__ e
 
-let test_gunzip_string () =
+let test_decompress_string () =
   let hex_input =
     "1f8b0808aa4fec56000368656c6c6f00f348cdc9c957e402009ed842b007000000"
   in
   let input = Util_hex.decode hex_input in
   let expected_output = "Hello!\n" in
-  gunzip_string input = expected_output
+  decompress_string input = expected_output
 
-let test_gzip_string () =
+let test_compress_string () =
   let input = "Hello, world." in
-  let z = gzip_string input in
-  let input' = gunzip_string z in
+  let z = compress_string input in
+  let input' = decompress_string z in
   input = input'
 
 let tests = [
-  "gunzip string", test_gunzip_string;
-  "gzip string", test_gzip_string;
+  "decompress string", test_decompress_string;
+  "compress string", test_compress_string;
 ]
