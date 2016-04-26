417f324c2716712868e54f754169ca20171b9a5e
Martin Jambon <martin@mjambon.com>
2016-04-26 14:28:02 -0700

    gzip_string function

--- util_gzip.ml
+++ util_gzip.ml
@@ -6,8 +6,9 @@ let gunzip_string s =
   if String.length s > 100_000_000 then
     invalid_arg "Util_gzip.gunzip_string: input too large";
   let filename = Filename.temp_file "wolverine-" ".gz" in
-  BatPervasives.output_file ~filename ~text:s;
+  let finally () = Sys.remove filename in
   try
+    BatPervasives.output_file ~filename ~text:s;
     let ic = Gzip.open_in filename in
     let acc = Buffer.create (3 * String.length s) in
     let max_chunk_len = 8192 in
@@ -20,10 +21,28 @@ let gunzip_string s =
       )
     in
     read_loop ();
-    Sys.remove filename;
-    Buffer.contents acc
+    let result = Buffer.contents acc in
+    finally ();
+    result
   with e ->
-    Sys.remove filename;
+    finally ();
+    Trax.raise __LOC__ e
+
+let gzip_string s =
+  (* some arbitrary limit on input size *)
+  if String.length s > 1_000_000_000 then
+    invalid_arg "Util_gzip.gzip_string: input too large";
+  let filename = Filename.temp_file "wolverine-" ".gz" in
+  let finally () = Sys.remove filename in
+  try
+    let oc = Gzip.open_out filename in
+    Gzip.output oc s 0 (String.length s);
+    Gzip.close_out oc;
+    let result = BatPervasives.input_file ~bin:true filename in
+    finally ();
+    result
+  with e ->
+    finally ();
     Trax.raise __LOC__ e
 
 let test_gunzip_string () =
@@ -34,6 +53,13 @@ let test_gunzip_string () =
   let expected_output = "Hello!\n" in
   gunzip_string input = expected_output
 
+let test_gzip_string () =
+  let input = "Hello, world." in
+  let z = gzip_string input in
+  let input' = gunzip_string z in
+  input = input'
+
 let tests = [
   "gunzip string", test_gunzip_string;
+  "gzip string", test_gzip_string;
 ]
